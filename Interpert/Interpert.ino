498173b9d8a55e48bdedd929f9bd4686b229f23c
Sounder317 <phelpsag05@gmail.com>
2023-09-29 17:24:03 -0400

    Update Interpert.ino

--- Interpert/Interpert.ino
+++ Interpert/Interpert.ino
@@ -1,8 +1,9 @@
 //code
-//Define the values for the stick on reciver and encoder 
+//Define the values for the stick on reciver 
 #define CH1 3
 #define CH2 5
-int encoder = A0
+//is for encoder
+int encoderp = A3;
 //ch1 is X direction 
 int ch1Value;
 //ch2 is y direction 
@@ -15,6 +16,14 @@ int enc_pos_deg;
 //wanted position of wheel
 int w_pos_rad;
 int w_pos_deg;
+
+//options
+int op1;
+int op2val;
+int op2;
+
+//wheel direction
+int direct=1;
 
 // Read the number of a specified channel and convert to the range provided.
 // If the channel is off, return the default value
@@ -31,12 +40,13 @@ void setup() {
   // Set all pins as inputs
   pinMode(CH1, INPUT);
   pinMode(CH2, INPUT);
+  
 
 }
 
 void loop() {
   //get encoder position in raw number
-  enc_pos_1=analogRead(enconder);
+  enc_pos_1 = analogRead(encoderp);
 
   //map to degrees
   enc_pos_deg=map(enc_pos_1, 0, 1023, 0, 360);
@@ -46,9 +56,20 @@ void loop() {
   ch2Value = readChannel(CH2, -100, 100, 0);
 
   //translate transmitted vaules to usful infromation 
-  w_pos_rad=atan(ch1Value/ch2Value);
+  w_pos_rad=atan(ch2Value/ch1Value);
   w_pos_deg=(w_pos_rad*(180/3.14));
 
-  //set wheel direction
+//math in terms of which dirction for wheel to turn
+  op1=(enc_pos_deg-w_pos_deg);
+  op2val=abs(w_pos_deg-180);
+  op2=(enc_pos_deg-op2val);
+  if(abs(op1)<=abs(op2)){
+    direct=1;
+  }
+  else{
+    direct=-1;
+  }
 
+//where to turn 
+  
 }
